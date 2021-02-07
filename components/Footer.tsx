14813b3fcce28b38f7b8d76ba54d06bdfcac4910
harfire <harcalm@gmail.com>
2021-02-07 16:45:37 +0700

    feat(location-list): add header and footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="columns is-multiline">
+      <div className="column is-full">
+        <p className="has-text-centered is-size-7">&#169; Haris Rahman</p>
+      </div>
+    </footer>
+  );
+}
